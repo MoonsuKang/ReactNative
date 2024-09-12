1e85b8725740b96729dbff12826f49efbddb2b48
Moonsu Kang <rkdms4971@naver.com>
2024-09-12 17:50:54 +0900

    andorid & ios 리플효과

--- components/GoalItem.js
+++ components/GoalItem.js
@@ -2,11 +2,17 @@ import { StyleSheet, View, Text, Pressable } from 'react-native'
 
 function GoalItem(props) {
     return (
-        <Pressable onPress={props.onDeleteItem.bind(this, props.id)}>
-            <View style={styles.goalItems}>
+        <View style={styles.goalItems}>
+            <Pressable 
+            android_ripple={{ color: "#dddddd" }}
+            onPress={props.onDeleteItem.bind(this, props.id)}
+            style={({pressed}) => pressed && styles.pressedItem}
+            >
+
                 <Text style={styles.goalText}>{props.text}</Text>
-            </View>
-        </Pressable>
+
+            </Pressable>
+        </View>
     );
 }
 export default GoalItem;
@@ -14,11 +20,14 @@ export default GoalItem;
 const styles = StyleSheet.create({
     goalItems: {
         margin: 8,
-        padding: 8,
         borderRadius: 6,
         backgroundColor: '#5e0acc',
     },
+    pressedItem: {
+        opacity: 0.5
+    },
     goalText: {
         color: 'white',
+        padding: 8,
     },
 });
